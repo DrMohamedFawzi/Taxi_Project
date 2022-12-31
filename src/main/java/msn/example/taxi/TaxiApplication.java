651c8add0ca5f20634958693a522f1f28f365fb7
DrMohamedFawzi <94241270+DrMohamedFawzi@users.noreply.github.com>
2022-12-31 19:28:31 +0200

    نسال الله التمام

---
+++ src/main/java/msn/example/taxi/TaxiApplication.java
@@ -0,0 +1,13 @@
+package msn.example.taxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/msn/example/taxi/TaxiApplicationTests.java
@@ -0,0 +1,13 @@
+package msn.example.taxi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
